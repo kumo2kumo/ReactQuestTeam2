5aa49bf17338addde9281c49e67170073d7af9e1
masanori0117 <masanori.takahashi.0117@gmail.com>
2021-02-07 20:31:42 +0900

    GETメソッドにてZOOMAPIから情報取得のトライアル

--- team2/backend/config.js
+++ team2/backend/config.js
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || 'production'
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
 	development :{
-		APIKey : 'nJTtqXAKTFWW5Ttx85_Lvw',
-		APISecret : 'FLbywctnr7pO1vyQ8Vr333MrqkBHFkK4TRJw'
+		APIKey : '',
+		APISecret : ''
 	},
 	production:{
-		APIKey : 'nJTtqXAKTFWW5Ttx85_Lvw',
-		APISecret : 'FLbywctnr7pO1vyQ8Vr333MrqkBHFkK4TRJw'
+		APIKey : '',
+		APISecret : ''
 	}
 };
 
