29635d287dd81cd5105acfe49262a99788bd2c7d
masanori0117 <masanori.takahashi.0117@gmail.com>
2021-02-07 20:37:03 +0900

    users.js 編集(GET METHOD TRIAL)

--- team2/backend/routes/users.js
+++ team2/backend/routes/users.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource aaa');
+// });
 
-module.exports = router;
+// module.exports = router;
+
+const request = require('request-promise');
+// const url = 'https://google.com';
+const url = 'https://api.zoom.us/v2/meetings/{72923573760}'
+const options = {
+  url: url,
+  method: 'GET',
+  json: true,
+  headers: {
+    'User-Agent': 'Zoom-Jwt-Request',
+    'content-type': 'application/json'
+  },
+  resolveWithFullResponse: true,
+  simple: true,
+};
+
+const get_api = (req, res, next) => {
+  request(options, function(error, response, body) {
+  console.log(body);
+  console.log(response);
+  console.log(error);
+  res.send(body);
+  // res.send(response);
+})};
+
+module.exports = get_api;
+
