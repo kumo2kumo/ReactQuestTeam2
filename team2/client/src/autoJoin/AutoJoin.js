f52e77239b0f14af54912dc82f0cf46006c4143a
kumo2kumo <amgokuraku@gmail.com>
2021-02-09 01:20:34 +0900

    FIXME: setTimeIntervalの入れ子、clearTimeOut効かない

--- team2/client/src/autoJoin/AutoJoin.js
+++ team2/client/src/autoJoin/AutoJoin.js
@@ -1,31 +1,42 @@
-import React from "react";
-
-const start_time = new Date("2021-02-08T15:30:00Z"); //開始時間入れる
-let is_started = false;
-
-//! 現在時間をカウントしていく
-const prsentTImeTimer = setInterval(() => {
-  const dt = new Date();
-  const joinTImer = setInterval(autoJoin, 20 * 1000, dt, joinTImer); //return timerID  
-}, 60 * 1000);
-
+// １つのカードコンポーネントに入れるの想定
 // TODO componentDidMount()に設定 
 
-const autoJoin = (dt, timer) => {
-  //もし現時刻==data[1]なら
-    if(dt.getMonth() === start_time.getMonth() &&
-      dt.getDate() === start_time.getDate() &&
-      dt.getHours() === start_time.getHours() &&
-      dt.getMinutes() === start_time.getMinutes() 
-    ) {
-        window.open("https://ja.reactjs.org/docs/state-and-lifecycle.html");//URL入れる
-        is_started = true;
-        clearInterval(timer);
-      }
-  console.log('func; '+ is_started);
-};
+import React from "react";
 
-// const joinTImer = setInterval(autoJoin, 20 * 1000, dt); //return timerID  
+const start_time = new Date("2021-02-08T16:16:00Z"); //開始時間入れる
+let is_started = false;
+
+//現在時間を30秒ごとに更新していく
+const prsentimeTimer = setInterval(() => {
+  
+    const dt = new Date();
+    const joinTImer = setInterval(joinCheck, 20 * 1000, dt); //return timerID  
+    console.log(dt);
+  //60秒ごとに現在タイムと開始時間が合うかチェック
+ 
+  // ! 停止が効いてない
+  if(is_started){
+    clearInterval(joinTImer);
+    clearInterval(prsentimeTimer);
+  }
+}, 20 * 1000);
+
+  
+  const joinCheck = (dt) => {
+    //もし現時刻==data[1]なら
+      if(dt.getMonth() === start_time.getMonth() &&
+        dt.getDate() === start_time.getDate() &&
+        dt.getHours() === start_time.getHours() &&
+        dt.getMinutes() === start_time.getMinutes() 
+      ) {
+          window.open("https://ja.reactjs.org/docs/state-and-lifecycle.html");//URL入れる
+          is_started = true;
+          // clearInterval(joinTImer);
+        }
+    console.log('func; '+ is_started);
+    };
+    
+  // const joinTImer = setInterval(joinCheck, 20 * 1000, dt); //return timerID  
 
 // Leave: もし現時刻==data[2]ならALT + Q -> enter
 
