0988a911d01321a5191733bcdbf0c686fbbb639b
kumo2kumo <amgokuraku@gmail.com>
2021-02-09 01:51:24 +0900

    autoJoinTimer仮完成

--- team2/client/src/autoJoin/AutoJoin.js
+++ team2/client/src/autoJoin/AutoJoin.js
@@ -1,50 +1,35 @@
 // １つのカードコンポーネントに入れるの想定
-// TODO componentDidMount()に設定 
-
 import React from "react";
 
-const start_time = new Date("2021-02-08T16:16:00Z"); //開始時間入れる
+const start_time = new Date("2021-02-08T17:53:00Z"); //開始時間入れる
 let is_started = false;
 
-//現在時間を30秒ごとに更新していく
-const prsentimeTimer = setInterval(() => {
-  
-    const dt = new Date();
-    const joinTImer = setInterval(joinCheck, 20 * 1000, dt); //return timerID  
-    console.log(dt);
-  //60秒ごとに現在タイムと開始時間が合うかチェック
- 
-  // ! 停止が効いてない
-  if(is_started){
-    clearInterval(joinTImer);
-    clearInterval(prsentimeTimer);
-  }
-}, 20 * 1000);
-
-  
-  const joinCheck = (dt) => {
-    //もし現時刻==data[1]なら
-      if(dt.getMonth() === start_time.getMonth() &&
-        dt.getDate() === start_time.getDate() &&
-        dt.getHours() === start_time.getHours() &&
-        dt.getMinutes() === start_time.getMinutes() 
-      ) {
-          window.open("https://ja.reactjs.org/docs/state-and-lifecycle.html");//URL入れる
-          is_started = true;
-          // clearInterval(joinTImer);
-        }
+//現在時間を60秒ごとに更新していく = 開始時間minutes + 1から対象
+const autoJoinTimer = setInterval(() => {
+  const dt = new Date();
+  console.log(dt);
+  if( dt.getMonth() === start_time.getMonth() &&
+      dt.getDate() === start_time.getDate() &&
+      dt.getHours() === start_time.getHours() &&
+      dt.getMinutes() === start_time.getMinutes() 
+  ) {
+      window.open("https://ja.reactjs.org/docs/state-and-lifecycle.html");//URL入れる
+      is_started = true;
+      clearInterval(autoJoinTimer);
+    }
     console.log('func; '+ is_started);
-    };
-    
-  // const joinTImer = setInterval(joinCheck, 20 * 1000, dt); //return timerID  
+}, 60 * 1000);
 
 // Leave: もし現時刻==data[2]ならALT + Q -> enter
 
 class AutoJoin extends React.Component {
+  componentDidMount() {
+    // autoJoinTimer呼び込む
+  }
   render () {
     return (
       <div>
-        Autojoin
+        zoom予定カード
         <section>
           <p>zoom meeting url</p>
           <p>zoom meeting 開始時間</p>
