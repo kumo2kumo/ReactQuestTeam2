a3aa29d90775daff25788901a0f039d6d1f51e92
kumo2kumo <amgokuraku@gmail.com>
2021-02-15 20:38:13 +0900

    Improve: initial state, storeセット

--- team2/client/src/features/cards/cardsSlice.js
+++ team2/client/src/features/cards/cardsSlice.js
@@ -1,7 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 // fake data
-const initialState = []
+const initialState = [
+    { id: '1', title: 'test1', memo: 'new', url: '111', stare_time: '17:00', owner: 'aaa', meetingId: '123', delete: false},
+    { id: '2', title: 'test2', memo: 'new2', url: '222', stare_time: '18:00', owner: 'bbb', meetingId: '456', delete: false}
+]
 
 const cardsSlice = createSlice({
     name: 'cards',
