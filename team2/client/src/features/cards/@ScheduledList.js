a0ddc331e7d3be7ae5c8187565b26fc7248728aa
kumo2kumo <amgokuraku@gmail.com>
2021-02-15 23:58:17 +0900

    ADD: card追加のReducer追加

---
+++ team2/client/src/features/cards/@ScheduledList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+// TODO 仮 card componentに埋め込み
+// export const ScheduledList = () => {
+//   const cards = useSelector(state => state.cards)
+
+//   const renderdCardsLists = cards.map(card => (
+//     return <Card key={card.id} card={card} />
+//   ))
+//   return (
+//     <div>
+//       {renderdCardsLists}
+//     </div>
+//   )
+// }
+
+//お試し
+// export const ScheduledList = () => {
+//   const cards = useSelector(state => state.cards)
+//   const renderCards = cards.map(card => (
+//     <div key={card.id}>
+//       <p>{card.title}</p>
+//       <p>{card.url}</p>
+//     </div>
+//   ))
+//   return (
+//     <div>
+//       {renderCards}
+//     </div>
+//   )
+// }
+
--- team2/client/src/features/cards/cardsSlice.js
+++ team2/client/src/features/cards/cardsSlice.js
@@ -1,15 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 // fake data
-const initialState = [
+const initialState = {
+    cards: [
     { id: '1', title: 'test1', memo: 'new', url: '111', stare_time: '17:00', owner: 'aaa', meetingId: '123', delete: false},
     { id: '2', title: 'test2', memo: 'new2', url: '222', stare_time: '18:00', owner: 'bbb', meetingId: '456', delete: false}
-]
+    ],
+    status: 'idle',
+    error: null
 
+}
 const cardsSlice = createSlice({
     name: 'cards',
     initialState,
-    reducers: {}
+    reducers: {
+        // TODO 仮
+        addCard(state, action) {
+            state.posts.push(action.payload)
+        }
+    },
+    extraReducers: {
+        [addCardByapi.pending]: (state, action) => {
+            state.status = 'pending'
+        },
+        [addCardByapi.fulfilled]: (state, action) => {
+            state.status = 'success'
+            //TODO cardsArrayに追加
+            state.cards = state.posts.
+        },
+        [addCardByapi.rejected]: (state, action) => {
+            state.status = 'failed',
+            state.error = action.error.message
+        }
+    }
 })
 
-export default cardsSlice.reducer;
+// TODO apiserverとのやりとり→dispatchするためのaction creator
+// (dispatch(getCardInfo))でdispathできる
+//状態がpending, fulfilled, rejectedの3段階に分かれる
+export const getCardInfo = createAsyncThunk('cards/addCardByapi', async () => {
+    const respone = await axios.get('url')
+    return Response.data
+    //TODO payload作成
+})
+
+//TODO selector記入
+export const { addCard } = cardsSlice.actions
+export default cardsSlice.reducer
