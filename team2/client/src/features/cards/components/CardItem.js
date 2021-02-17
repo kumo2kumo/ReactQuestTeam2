1a9063b0e6d16ce968992e24319a2357936440e5
kumo2kumo <cgbk10@gmail.com>
2021-02-18 00:05:31 +0900

    FIX: 色変更

--- team2/client/src/features/cards/components/CardItem.js
+++ team2/client/src/features/cards/components/CardItem.js
@@ -8,14 +8,17 @@ const useStyles = makeStyles({
     root: {
         fontFamily: ['sans-serif', 'Raleway'],
         width: '90%',
+        borderRadius: '1rem',
+        // TODO 色目的に合わせて変えられたらいい https://codepen.io/suez/pen/eNwwGp
+        borderLeft: '8px solid #F7AA17',
         margin: '0.5rem 0rem 1rem 1.5rem',
         '&:hover': {
-            boxShadow: '0 12px 15px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19)'
+            boxShadow: '0 12px 15px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19)',
         }
     },
     icon: {
         marginRight: '0.5rem',
-    }
+    },
 })
 
 
@@ -25,33 +28,37 @@ export const CardItem = () => {
     return (
         <Card className={classes.root}>
             <CardContent>
-            <Typography variant="h5" gutterBottom>
-                hi there
-            </Typography>
-            <Typography color="textSecondary">
-                meetingID: 3432423
-            </Typography>
-            <Typography color="textSecondary">
-                <AccountCircleTwoToneIcon 
-                fontSize="small" 
-                className={classes.icon}
-                />
-                amam:gemil.com
-            </Typography>
-            <Typography color="textSecondary">
-                <AlarmTwoToneIcon 
-                fontSize="small" 
-                className={classes.icon}
-                />
-                17:30
-            </Typography>
-            <Link href="https://www.google.com" underline="hover">
-                <AddToQueueTwoToneIcon
-                fontSize="small" 
-                className={classes.icon}
-                />
-                https://www.google.com
-            </Link>
+                <Typography
+                variant="h5"
+                gutterBottom 
+                style={{borderBottom: '1px solid #000'}}>
+                    hi there
+                </Typography>
+                <Typography color="textSecondary">
+                    meetingID: <span style={{fontWeight: 'bolder'}}>3432423</span>
+                </Typography>
+                <Typography color="textSecondary">
+                    <AccountCircleTwoToneIcon 
+                    fontSize="small" 
+                    className={classes.icon}
+                    />
+                    owner
+                    <span style={{fontWeight: 'bolder', marginLeft: '1rem'}}>amer@gemil.com  </span>
+                </Typography>
+                <Typography color="textSecondary">
+                    <AlarmTwoToneIcon 
+                    fontSize="small" 
+                    className={classes.icon}
+                    />
+                    <span style={{fontWeight: 'bolder'}}> 17:30</span>
+                </Typography>
+                <Link href="https://www.google.com" underline="hover">
+                    <AddToQueueTwoToneIcon
+                    fontSize="small" 
+                    className={classes.icon}
+                    />
+                    https://www.google.com
+                </Link>
             </CardContent>
         </Card>
     )
